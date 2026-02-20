47f6a419f3db14d97c7171106833ce35ceef6b51
Kumar <kumarnaidukarri2002@gmail.com>
2026-02-20 20:01:25 +0530

    2.2 - Updated 'Navbar', open/close menu icon functionality, added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+import { useLocation } from "react-router";
+
 // my components
 import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith("/owner"); // true or false
+
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      {
+        // show navbar in all pages. but, hide it only in 'owner' url path. '/owner'.
+        !isOwnerPath && <Navbar setShowLogin={setShowLogin} />
+      }
+    </>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,29 @@
 import { useState } from "react";
-import { Link, useLocation } from "react-router"; // React-Router library
+import { Link, useLocation, useNavigate } from "react-router"; // React-Router library
 
 // my components
 import { assets, menuLinks } from "../assets/assets.js"; // images, dummy data
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const location = useLocation(); // hook returns "current URL" as an object. ex: {hash:'',pathname:'/',key:'',search:'',state:null}
-  const [menuOpen, setMenuOpen] = useState(false); //  state variable to 'show/hide menu' in mobile screens, laptop devices.
+  const navigate = useNavigate(); // hook to navigate to different URL paths.
+  const [menuOpen, setMenuOpen] = useState(false); // state variable to 'show/hide menu' in mobile screens, laptop devices.
 
   return (
     <div
       className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all   ${location.pathname === "/" && "bg-light"}`}
     >
+      {/* Logo */}
       <Link to="/">
         <img src={assets.logo} className="h-8" alt="logo" />
       </Link>
 
+      {/* Menu container */}
       <div
-        className={`menu-container  max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border border-borderColor right-0  flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4   transition-all duration-300 z-50   ${location.pathname === "/" ? "bg-light" : "bg-white"}   ${menuOpen === true ? "max-sm:translate-x-0" : "max-sm:translate-x-full"}`}
+        className={`menu-container  max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t  right-0  flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4   transition-all duration-300 z-50   ${location.pathname === "/" ? "bg-light" : "bg-white"}   ${menuOpen === true ? "max-sm:translate-x-0" : "max-sm:translate-x-full"}`}
       >
         {
-          // Map on each 'link item'
+          // Map on each 'menu item'
           menuLinks.map((menuLink, index) => {
             return (
               <Link key={index} to={menuLink.path}>
@@ -29,7 +32,42 @@ const Navbar = () => {
             );
           })
         }
+
+        {/* Search input box container */}
+        <div className="search-inputbox-container   hidden lg:flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full max-w-56">
+          <input
+            type="text"
+            className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
+            placeholder="Search Products"
+          />
+          <img src={assets.search_icon} alt="search" />
+        </div>
+
+        {/* Dashboard container*/}
+        <div className="dashboard-container   flex max-sm:flex-col items-start sm:items-center gap-6">
+          <button className="cursor-pointer" onClick={() => navigate("/owner")}>
+            Dashboard
+          </button>
+          <button
+            className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg"
+            onClick={() => setShowLogin(true)}
+          >
+            Login
+          </button>
+        </div>
       </div>
+
+      {/* Menu open close icon */}
+      <button
+        className="sm:hidden cursor-pointer"
+        aria-label="Menu"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <img
+          src={menuOpen === true ? assets.close_icon : assets.menu_icon}
+          alt="menu"
+        />
+      </button>
     </div>
   );
 };
