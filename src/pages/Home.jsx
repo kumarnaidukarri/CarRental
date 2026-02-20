c34d1a55efd4d812f04e452ccb67818be09d98a1
Kumar <kumarnaidukarri2002@gmail.com>
2026-02-21 00:50:02 +0530

    3.3 - added 'Feature Vehicles' section in Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@
 
 // my components
 import Hero from "../components/Hero.jsx";
+import FeaturedSection from "../components/FeaturedSection.jsx";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <FeaturedSection />
     </>
   );
 };
