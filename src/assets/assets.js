8ced25c498fd9b71e864f420edb02987a5b5a202
Kumar <kumarnaidukarri2002@gmail.com>
2026-02-25 00:19:51 +0530

    refactored code

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -125,7 +125,7 @@ export const ownerMenuLinks = [
 
 export const dummyUserData = {
   _id: "6847f7cab3d8daecdb517095",
-  name: "GreatStack",
+  name: "Mr Kumar",
   email: "admin@example.com",
   role: "owner",
   image: user_profile,
